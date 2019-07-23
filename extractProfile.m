3d3c9f393e0ea586437905b537772a7907427d75
OwlEyes18 <26035170@student.g.nwu.ac.za>
2019-07-23 17:21:34 +0200

    Creation of the custom profile ectraction function - only able to discern geo. approximation method.

---
+++ extractProfile.m
@@ -0,0 +1,52 @@
+%#######################################################################
+%#                                                                     #
+%#               EERI 474 - Profile Extraction Function                #
+%#                    by J. Koekemoer 26035170                         #
+%#                                                                     #
+%#######################################################################
+
+% The function used to retrieve distance and elevation between coordinates
+% IN: all key information
+% OUT: path profile plot distance and elevation values
+
+function [z_elev,r_dist] = extractProfile(tile_data,ref_mat,pointSet,stepSize,approxMethod,interpMethod)
+
+%% Temp Vars
+pointSet = [  -14.9100   13.5000
+             -14.9300   13.4800];
+
+
+%% Code Begins
+
+%% Check Geo. Approx. Method & Get Distance
+
+approxMethod = lower(approxMethod); %Text to lowercase for some error prevention
+
+if approxMethod == 'flat'
+    {
+        disp('This Geographical Approximation Method Is Not Yet Supported \n');
+        % arclen = sqrt((x1-x0)^2 + (y1-y069)^2);
+    }
+elseif approxMethod == 'haversine'
+    {
+        disp('Haversine Geographical Approximation Method');
+        
+        [arclen, azimuth] = distance(pointSet(1,:), pointSet(2,:));
+    }
+elseif approxMethod == 'vincenty'
+    {
+        disp('This Geographical Approximation Method Is Not Yet Supported \n');
+        %[arclen, azimuth] = distance(pointSet(1,:),
+        %pointSet(2,:),ellipsoid); % Add ellipsoid
+    }
+else
+    {
+        disp('Invalid Geographical Approximation Method');
+    }
+end
+
+
+%% Get Start/End Data "Cells"
+
+%% Get Elevations
+
