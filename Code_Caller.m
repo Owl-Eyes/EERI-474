c3f74854c43ad590d205b1ff52ea473869803aa2
OwlEyes18 <26035170@student.g.nwu.ac.za>
2019-07-23 17:20:00 +0200

    Edited a variable name for clarity, included place-holder PEPE call

--- Code_Caller.m
+++ Code_Caller.m
@@ -18,10 +18,10 @@ tile_name = 's15_e013_1arc_v3.tif'; % Lubango, Huila
 % plon = [13 13.25 13.5 13.75 14]; % Across
 % plat = [-15 -14.75 -14.5 -14.25 -14];
 
-plat = [-14.91 -14.92 -14.93]; % Town
-plon = [13.5 13.49 13.48];
+plat = [-14.91 -14.93]; % Town
+plon = [13.5 13.48];
 
-numSamples = 1000;
+stepSize = 1000; % Distance between samples
 
 interpMethod = 'bilinear';
 approxMethod = 'gc'; % Great circle
@@ -31,9 +31,9 @@ fileType = 'tif';
 %% 'Main'
 
 % Get profile data
-[distData, elevData] = PEPE(tile_name,plat,plon,numSamples,interpMethod,approxMethod,fileType);
-% Plot results in a figure
-plotProfile(distData, elevData);
+[distData, elevData] = PEPE(tile_name,plat,plon,stepSize,interpMethod,approxMethod,fileType);
+% Plot results in a figure (optional)
+%plotProfile(distData, elevData);
 
 
 %% Misc
