c73a58c86f9d9cc6024814ea9b6c0f2ae436183a
OwlEyes18 <26035170@student.g.nwu.ac.za>
2019-07-30 20:43:10 +0200

    Updated the distance calculation for the function for all three methods.

--- extractProfile.m
+++ extractProfile.m
@@ -16,40 +16,74 @@ function [z_elev,r_dist] = extractProfile(tile_data,ref_mat,pointSet,latRange,lo
 %              -14.9300   13.4800];
 
 % points      /```START```\/````END````\
-% pointSet = [-14.91 13.5 -14.93 13.48]; % Town
+pointSet = [-14.91 13.5 -14.93 13.48]; % Town
+%pointSet = [37 -76 67 -76]; %Test values in help function
 
-plats = [pointSet(1,:) pointSet(3,:)];
-plons = [pointSet(2,:) pointSet(4,:)];
+plats = [pointSet(1,1) pointSet(1,3)];
+plons = [pointSet(1,2) pointSet(1,4)];
+
+R = earthRadius('meters');
+
+numPairs = length(pointSet(:,1))
+
+currentBeginPair = [];
+currentEndPair = [];
 
 %% Code Begins
+% Get tile info and data
+[tile_data, tile_info, ref_mat] = getTileStuff(filePath);
+
+
+
 
 %% Check Geo. Approx. Method & Get Distance
 
 approxMethod = lower(approxMethod); %Text to lowercase for some error prevention
 
-if approxMethod == 'flat'
-    {
-        disp('This Geographical Approximation Method Is Not Yet Supported \n');
-        % arclen = sqrt((x1-x0)^2 + (y1-y069)^2);
-    }
-elseif approxMethod == 'haversine'
-    {
-        disp('Haversine Geographical Approximation Method');
-        
-        [arclen, azimuth] = distance(plats(1,:), plons(1,:));
-    }
-elseif approxMethod == 'vincenty'
-    {
-        disp('This Geographical Approximation Method Is Not Yet Supported \n');
-        %[arclen, azimuth] = distance(pointSet(1,:),
-        %pointSet(2,:),ellipsoid); % Add ellipsoid
-    }
-else
-    {
-        disp('Invalid Geographical Approximation Method');
-    }
-end
+for i = 1:numPairs
+    
+        currentBeginPair = [pointSet(i,1), pointSet(i,2)]
+        currentEndPair = [pointSet(i,3), pointSet(i,4)]
 
+        if (strcmp(approxMethod,'flat') == 1)
+            
+                disp('Flat Earth Geographical Approximation Method \n');
+               
+                % Latitude degrees to radians
+                phi1 = deg2rad(pointSet(i,1)); 
+                phi2 = deg2rad(pointSet(i,3));
+                
+                % Longitudes
+                lam1 = pointSet(i,2);
+                lam2 = pointSet(i,4);
+                
+                x = (lam2 - lam1) * cos((phi1+phi2)/2);  % Change in x
+                y = (phi2 - phi1);                       % Change in y
+                arclen = sqrt((x)^2 + (y)^2) * R        % Distance
+            
+        elseif (strcmp(approxMethod,'haversine') == 1)
+            
+                disp('Haversine Geographical Approximation Method');
+
+                [arclen, azimuth] = distance(currentBeginPair, currentEndPair)
+                kms = deg2km(arclen)    % to kilometers
+                
+        elseif (strcmp(approxMethod,'vincenty') == 1)
+            
+                disp('Vincenty Geographical Approximation Method');
+                
+                e = referenceEllipsoid(tile_info.Ellipsoid);
+                [arclen, azimuth] = distance(currentBeginPair, currentEndPair, e)
+                kms = deg2km(arclen)    % to kilometers
+            
+        else
+            
+                disp('Invalid Geographical Approximation Method');
+            
+        end
+        
+          
+end
 
 %% Get Start/End Data "Cells"
 
