7b41c249ad7360f253e99c077eba1272e998c26b
OwlEyes18 <26035170@student.g.nwu.ac.za>
2019-08-27 17:40:00 +0200

    GTOPO30 file format now supported. Removed return of ref_matp.

--- getTileStuff.m
+++ getTileStuff.m
@@ -8,29 +8,48 @@
 % This function reads the tile file, and stores all useful information
 % and data in a format usable by the main code and other sub-functions.
 % IN: the file path and name together
-% OUT: the dem data, metadata, and reference matrix
+% OUT: the dem data, metadata, reference matrix, and coordinate ranges
 
-function [tileData, tileInfo, refMat, refMatg] = getTileStuff(tileName)
+function [tileData, e, refMat, lat_range, long_range] = getTileStuff(tileName)
 
 %tileName = '9129CATD.ddf';
 
 % Determine file extension
 lowerTile = lower(tileName);
 tileExtension = extractAfter(lowerTile,'.');
+tileNameOnly = extractBefore(tileName,'.');
 
 % Sort file formats
 if (strcmp(tileExtension,'tif') || strcmp(tileExtension,'tiff')) %GeoTiff files
     
-    [tileData, refMat] = geotiffread(tileName); % get tile data and reference matix
+    [tileData, refMatp] = geotiffread(tileName); % get tile data and reference matix
     tileInfo = geotiffinfo(tileName);           % get tile metadata
     tileData = double(tileData);                % Convert data to doubles
     % Geographical objects
-    refMatg = georefcells(refMat.LatitudeLimits,refMat.LongitudeLimits,refMat.RasterSize);
-
+    refMat = georefcells(refMatp.LatitudeLimits,refMatp.LongitudeLimits,refMatp.RasterSize);
+    
+    [lat_range, long_range] = dispDEMInfo(refMat);
+    
+    e = referenceEllipsoid(tileInfo.Ellipsoid);
+    
 elseif strcmp(tileExtension,'ddf') % SDTS files
-    [tileData, refMatg] = sdtsdemread(tileName); % get tile data and reference matix
+    [tileData, refMat] = sdtsdemread(tileName); % get tile data and reference matix
     tileInfo = sdtsinfo(tileName);              % get tile metadata
     tileData = double(tileData);                % Convert data to doubles
+    
+    [lat_range, long_range] = dispDEMInfo(refMat);
+    
+    
+elseif strcmp(tileExtension,'dem') % GTOPO30 files
+    [tileData,refvec] = gtopo30(tileNameOnly); % Include lat and long lims in future
+    rasterSize = size(tileData);
+    refMat = refvecToGeoRasterReference(refvec,rasterSize);
+    [lat_range, long_range] = dispDEMInfo(refMat);
+    
+    % Calculate the reference ellipsoid
+    UTM = utmzone(lat_range,long_range);
+    [ellip,ellipName] = utmgeoid(UTM);
+    e = referenceEllipsoid(ellipName);
     
 else
     errordlg('File not currently supported','File Error');
